36bd9334857912fa9343871738b64b498da79679
Mikhail Niedre <mikhail.niedre@quantori.com>
2024-12-14 13:51:28 +0300

    reply markup

--- README.md
+++ README.md
@@ -5,9 +5,16 @@ telegram bot implemented on spring boot
 Required environmental variables:
 ```dotenv
 TGBOT_TOKEN=
-TGBOT_OPENAI_TOKEN=
 TGBOT_PROXY_HOSTNAME=
 TGBOT_PROXY_PORT=
 TGBOT_PROXY_USERNAME=
 TGBOT_PROXY_PASSWORD=
+SPRING_AI_OPENAI_API_KEY=
+```
+
+## telegram commands
+```
+start - начало или сброс настроек
+markup - использовать разметку кода
+# repeat - повторить команду
 ```
--- src/main/java/cane/brothers/tgbot/TgBot.java
+++ src/main/java/cane/brothers/tgbot/TgBot.java
@@ -1,8 +1,11 @@
 package cane.brothers.tgbot;
 
-import cane.brothers.tgbot.openai.OpenaiClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.text.StringEscapeUtils;
+import org.apache.commons.text.translate.AggregateTranslator;
+import org.apache.commons.text.translate.LookupTranslator;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.longpolling.BotSession;
 import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
@@ -14,7 +17,10 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 
 @Slf4j
@@ -22,9 +28,23 @@ import java.util.concurrent.atomic.AtomicReference;
 @RequiredArgsConstructor
 public class TgBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
 
+    private final static Map<CharSequence, CharSequence> ESCAPE_MAP = new HashMap<>() {
+        {
+            put(".", "\\.");
+            put("!", "\\!");
+            put("(", "\\(");
+            put(")", "\\)");
+            put("#", "\\#");
+        }
+    };
+    private static final AggregateTranslator ESCAPE = new AggregateTranslator(
+            new LookupTranslator(Collections.unmodifiableMap(ESCAPE_MAP))
+    );
+
     private final TelegramClient telegramClient;
     private final TgBotProperties properties;
-    private final OpenaiClient openaiClient;
+    private final ChatClient chatClient;
+    private boolean useMarkup = false;
 
     @Override
     public String getBotToken() {
@@ -43,12 +63,41 @@ public class TgBot implements SpringLongPollingBot, LongPollingSingleThreadUpdat
 
     @Override
     public void consume(Update update) {
-        // messages
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            AtomicReference<SendMessage> reply = new AtomicReference<>();
-            reply.set(openaiClient.getReply(update));
-            logMessage(update, reply.get());
-            sendMessage(reply.get());
+        // command
+        if (update.hasMessage() && update.getMessage().isCommand()) {
+            if (update.getMessage().getText().equals("/markup"))
+                useMarkup = true;
+            else if (update.getMessage().getText().equals("/start"))
+                useMarkup = false;
+        }
+
+        // messages to gpt
+        else if (update.hasMessage() && update.getMessage().hasText()) {
+            Long chatId = update.getMessage().getChatId();
+            String prompt = update.getMessage().getText();
+
+            String answer;
+            try {
+                answer = chatClient.prompt()
+                        .user(prompt)
+                        .call()
+                        .content();
+            } catch (Exception ex) {
+                log.error("open-ai error: " + ex.getMessage(), ex);
+                answer = "Произошла ошибка при обработке запроса к open-ai.";
+            }
+
+            var msgBuilder = SendMessage.builder()
+                    .chatId(chatId);
+            if (useMarkup) {
+                msgBuilder.parseMode("MarkdownV2")
+                        .text(StringEscapeUtils.builder(ESCAPE).escape(answer).toString());
+            } else {
+                msgBuilder.text(Optional.ofNullable(answer).orElse("no answer"));
+            }
+            SendMessage reply = msgBuilder.build();
+            logMessage(update, reply);
+            sendMessage(reply);
         }
     }
 
@@ -57,6 +106,13 @@ public class TgBot implements SpringLongPollingBot, LongPollingSingleThreadUpdat
             telegramClient.execute(sendMessage);
         } catch (TelegramApiException e) {
             log.error("Can't send message to telegram", e);
+            try {
+                SendMessage fallbackMessage = SendMessage.builder().chatId(sendMessage.getChatId())
+                        .text("Произошла ошибка во время обрабоки. Попробуйте измений запрос.\n" + e.getMessage()).build();
+                telegramClient.execute(fallbackMessage);
+            } catch (TelegramApiException ex) {
+                log.error("Can't send fallback message to telegram", ex);
+            }
         }
     }
 
