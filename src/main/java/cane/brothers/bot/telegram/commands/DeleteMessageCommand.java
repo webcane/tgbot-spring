969f9040c15b473fdf35e12021b623fedb72ea64
Mikhail Niedre <mikhail.niedre@quantori.com>
2025-02-13 15:01:15 +0300

    quick reply

---
+++ src/main/java/cane/brothers/bot/telegram/commands/DeleteMessageCommand.java
@@ -0,0 +1,27 @@
+package cane.brothers.bot.telegram.commands;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.objects.message.Message;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+@Slf4j
+@Component("/delete")
+@RequiredArgsConstructor
+public class DeleteMessageCommand implements ChatCommand<Message> {
+
+    private final TelegramClient telegramClient;
+
+    @Override
+    public void execute(Message data) throws TelegramApiException {
+        var chatId = data.getChatId();
+
+        var reply = DeleteMessage.builder().chatId(chatId)
+                .messageId(data.getMessageId())
+                .build();
+        telegramClient.execute(reply);
+    }
+}
